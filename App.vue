6c377ee49576ed700978bb0e650aef30a95d931b
margueritebauchez <80228015+margueritebauchez@users.noreply.github.com>
2021-03-22 11:43:41 +0100

    checkbox

--- App.vue
+++ App.vue
@@ -1,12 +1,27 @@
-
-
-
 <template>
 <div id="app">
     <h1>TodoList</h1>
+
+
     <AjoutTodo @nouvellenote="ajouterNote"></AjoutTodo> 
-    <Supprimer v-for="(todo, index) in todos" v-bind:key="todo" v-on:remove="todos.splice(index, 1)"></Supprimer>
-    {{todos}}
+    <li v-for="todo in todos" v-bind:key="todo.name" >
+    <input type='checkbox' v-model="todo.completed">
+    {{todo.name}}
+    <label for="checkbox"> Completed ? {{ todo.completed }} </label>
+    <button v-on:click="deleteTodo(todo)">Supprimer</button>    
+    </li>
+        {{todos}}
+
+
+    <AjoutListe @nouvelleliste="ajouterListe"></AjoutListe> 
+    <li v-for="liste in listes" v-bind:key="liste" >
+    {{liste}}
+    <button v-on:click="deleteTodo(liste)">Supprimer</button>    
+
+    </li>
+
+    
+
 </div>
 
 
@@ -14,27 +29,42 @@
 
 <script>
 import AjoutTodo from './components/Ajout.vue'
-import Supprimer from './components/Note.vue'
+import AjoutListe from './components/AjoutListe.vue'
+//import AffTodo from './components/AffTodo.vue'
+
+
 
 export default {
   name: 'App',
+  props: ['todo'],
   components: {
     AjoutTodo,
-    Supprimer
+   // AffTodo,
+    AjoutListe
     },
 
     data(){
         return{
         todos:[
-            "tache1",
-            "tache2",
-            "tache3"
+            {name:"tache1",completed: false },
+            {name:"tache1",completed: false },            
+            {name:"tache1",completed: false },            
+            ],
+
+        listes:[
+            "liste1",
+            "liste2",
+            "liste3"
             ]
         }
     },
     methods:{
         ajouterNote(message){
-          this.todos.push(message);
+          this.todos.push({ name: message ,completed: false })
+        },
+        deleteTodo(todo){
+            let index = this.todos.indexOf(todo);
+            this.todos.splice(index,1);
         },
     }
 }
