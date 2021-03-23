f208970e7b6d3c0a9d60a5d4ac63d85df96d1315
margueritebauchez <80228015+margueritebauchez@users.noreply.github.com>
2021-03-23 20:34:19 +0100

    Add files via upload

---
+++ src/components/AjoutListe.vue
@@ -0,0 +1,25 @@
+<template>
+<p>Ajouter une liste</p>
+      <input type="text" v-model="interne" placeholder="Ex. Liste à faire demain"/>
+      <a href="#" @click="enregistementListe" v-if="interne != '' ">Ajouter une liste</a>
+        {{interne}}
+</template>
+<script>
+    export default {
+      name: 'AjoutListe',
+      props:[],
+      emits: ['nouvelleliste'],
+      data(){
+        return{
+          interne:'une liste'
+        }
+      },
+      methods:{
+        enregistementListe(){
+          this.$emit('nouvelleliste',this.interne)
+          this.interne= '';  
+        }
+      },
+    }
+
+</script>
---
+++ src/components/Connexion.vue
@@ -0,0 +1,38 @@
+<template>
+	<form action="#" @submit.prevent="identifiant()">
+	<label for= "utilisateur">Utilisateur:   </label>
+	<input type="text" v-model="utilisateur" placeholder="Nom/Prenom"/>
+	<br>
+	<label for= "motdepasse">Mot de passe: </label>
+	<input type="password" v-model="motdepasse" placeholder="123abc"/>
+	<br>
+	<button type="submit">Se connecter</button>
+	</form>
+</template>
+
+
+<script>
+
+    export default {
+      name: 'Connexion',
+      props:[],
+      components: {
+      },
+      data(){
+				return{
+					utilisateur:'',
+					motdepasse:'',
+				}
+			},
+      emits: ['submit'],
+      methods:{
+        identifiant(){
+					//event personnalisé
+					this.$emit('submit',{
+						utilisateur:this.utilisateur,
+						motdepasse:this.motdepasse,
+					})
+        }
+    }
+}
+</script>
